ff47f2fad7d4da8c3e15fcfcdc987c998fef18da
Zijie Li <zijie.li@outlook.com>
2022-04-02 19:17:24 +1100

    Use axios for http instead of async function to remove some unecpected behaviour. Add links between pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import Login2 from './Login'
+import Register2 from './Register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element = {<Login2/>} />
+          <Route path = "/register" element = {<Register2/>} />
+          <Route path = "/file" element = {<>Hello</>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
