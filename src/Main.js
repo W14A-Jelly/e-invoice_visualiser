9739cbadc553895437a1c6e453ed674cb7c675df
Zijie Li <zijie.li@outlook.com>
2022-04-13 14:38:53 +1000

    Implement the funciton on main page. Now retrieve start/end are automatically. And rerendering for new invocies without page reloading

--- src/Main.js
+++ src/Main.js
@@ -31,97 +31,64 @@ const CusButton = styled(Button)({
     width: '110px',
 })
 
-let itemData = []
 
 
-class Main extends React.Component {
-    render() {
-    return (
-        <div className='background' style={{backgroundColor: '#90caf9', height: '100vh', display: 'grid', width: '100%', overflowX:'hidden', overflowY:'hidden', zIndex:0}}>
-            <div className='right_panel' style={{display: 'flex', position:'relative', alignItems: 'center', justifyContent:'center', left:'150px'}}>
-                <div className='filter' style={{position:'relative', bottom:'450px', left:'100px', zIndex:3}}>
-                    <FormGroup>
-                        <FormControlLabel control={<Switch color="warning"/>} label="Filter" />
-                    </FormGroup>
-                </div>
-                <Box component="span" sx={{width: '90vh', height: '70vh', backgroundColor: 'white', zIndex:1}}>
-                <div style={{fontSize: '25px', position:'relative', right:'450px', top:'50px', zIndex:4}}>Invoices</div>
-                <ImageList sx={{ width: '80vh', height: '60vh', position:'relative', left:'60px', top:'50px'}} cols={10}>
-                    {itemData.map((item) => (
-                        <ImageListItem key={item.title}>
-                        <IconButton sx={{border: "1px solid grey", borderRadius: 1, height: '100px'}}>
-                            <ArticleIcon fontSize="large" />
-                        </IconButton>
-                        <ImageListItemBar
-                            title={item.title}
-                            position="below"
-                        />
-                        </ImageListItem>
-                    ))}
-                    </ImageList>
-                </Box>
-            </div>
-            <div className='left_panel' style={{display: 'flex',  justifyContent:'left', alignItems:'center', position:'absolute'}}>
-                <Box component="span" sx={{ width: 300, height: '100vh', backgroundColor: 'white', margin:'0', padding:'0'}}>
-                    <div style={{position:'aboslute', top:'20px'}}>
-                    <img src={logo} alt="logo"  width="150" height="100" />
-                    </div>
-                    <div className='list'>
-                        <List sx={style} component="nav" aria-label="mailbox folders">
-                        <ListItem button>
-                            <ListItemText  primary="Profile" />
-                        </ListItem>
-                        <Divider />
-                        <ListItem button divider>
-                            <ListItemText primary="Invoices" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemText primary="Reports" />
-                        </ListItem>
-                        <Divider  light />
-                        <ListItem button>
-                            <ListItemText primary="Blacklist" />
-                        </ListItem>
-                        </List>
-                    </div>
-                    <div className='logout' style={{position:'relative', top:'100px'}}>
-                        <CusButton variant="contained" color="error">Log out</CusButton>
-                    </div>
-                </Box>
-            </div>
-        </div>
-        );
-    }
-}
 
 const File = () => {
-    const [files, setfiles] = useState([])
+    
     const [btnState, setBtn] = useState(false)
+    const [itemData,setitemData] = useState([])
 
+    // Happens every 10 seconds. Get new list of invoices names
 	useEffect (() =>{
-		const get_files = async () =>{
-			const filesfromserver = await fetchfiles()
-			setfiles(filesfromserver)
-		}
-	get_files()
-
+		setInterval(() => {
+            fetchfiles() 
+            
+        },5000)
 	}, [])
 
-	const fetchfiles = async () => {
-        //change url later
+
+
+
+
+    //Happens at page loading
+    useEffect (() =>{
+        fetchfiles()
+        handleend()
+        handlestart()
+	}, [])
+
+
+    function fetchfiles() {
         const url = 'https://damp-sands-01446.herokuapp.com/list/filenames?token='+localStorage.token;
-        //const url = 'http://192.168.1.184:8080/example'
-        console.log(url);
-		const response = await fetch(url);
-		//const response = await fetch('http://192.168.1.184:8080/example?token='+localStorage.token)
-		const data = (await response.json()).filenames
-		return data
-	}
-	itemData = []
-    for (const x in files) {
-        itemData = [...itemData,{title:files[x]}]
+        
+        const options = {headers : {'Content-type': 'application/json'}}
+        axios({method: "get", url:url})
+        .then((response) => {
+            console.log(response)
+            const data = response.data.filenames;
+            
+            let New_invoices = []
+            for (const x in data) {
+                New_invoices = ([...New_invoices,{title:data[x]}])
+            }
+            setitemData(New_invoices)
+            
+            return data;
+        })
+        .catch((err)=>{ })
     }
 
+	// const fetchfiles = async () => {
+    //     //change url later
+    //     const url = 'https://damp-sands-01446.herokuapp.com/list/filenames?token='+localStorage.token;
+    //     //const url = 'http://192.168.1.184:8080/example'
+	// 	const response = await fetch(url);
+	// 	//const response = await fetch('http://192.168.1.184:8080/example?token='+localStorage.token)
+	// 	const data = (await response.json()).filenames
+    //     console.log(response)
+	// 	return data
+	// }
     function go_logout(event) {
         window.location.href = ('/')
         localStorage.token = ''
@@ -137,11 +104,11 @@ const File = () => {
 
     function go_invoice(event) {
         window.location.href = ('/file')
+        
     }
 
 
-    function handlestart(event) {
-        event.preventDefault();
+    function handlestart() {
         //valid if empty
         const url = 'https://damp-sands-01446.herokuapp.com/email/retrieve/start';
         const data = JSON.stringify({token:localStorage.token});
@@ -156,8 +123,7 @@ const File = () => {
         
       }
 
-      function handleend(event) {
-        event.preventDefault();
+      function handleend() {
         //valid if empty
         const url = 'https://damp-sands-01446.herokuapp.com/email/retrieve/end';
         const data = JSON.stringify({token:localStorage.token});
@@ -168,7 +134,7 @@ const File = () => {
             const data = response.data;
             setBtn(false);
           })
-          .catch((err)=>{ })
+          .catch((err)=>{})
         
       }
     return (
@@ -222,10 +188,10 @@ const File = () => {
                         </List>
                     </div>
                     <div className='Start retrieve' style={{position:'relative', top:'100px'}}>
-                        <CusButton variant="contained" disabled={btnState} color="success" onClick = {handlestart}>Start retrieve</CusButton>
+                        <CusButton variant="contained" color="success">refresh</CusButton>
                     </div>
                     <div className='End retrieve' style={{position:'relative', top:'130px'}}>
-                        <CusButton variant="contained" disabled={!btnState} color="error" onClick = {handleend}>End retrieve</CusButton>
+                        <CusButton variant="contained" color="error">End retrieve</CusButton>
                     </div>
                     <div className='logout' style={{position:'relative', top:'700px'}}>
                         <CusButton variant="contained" onClick = {go_logout}>Log out</CusButton>
